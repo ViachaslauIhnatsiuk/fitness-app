b357eb5e153dff50adbffa2466d32b900c8190f7
GlebKolyano <glebkolyano38@gmail.com>
2022-08-17 13:54:54 +0300

    refactor: refactor routing and navbar links

--- src/components/UI/foodButton/FoodButton.tsx
+++ src/components/UI/foodButton/FoodButton.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
+import { IoFastFood } from 'react-icons/io5';
 import s from './FoodButton.module.css';
-import { ReactComponent as Food } from '../../../assets/navigation/food.svg';
 
 const FoodButton: FC = () => {
   return (
     <div className={s.wrapper}>
-      <Food className={s.icon} />
+      <h3 className={s.icon}>
+        <IoFastFood />
+      </h3>
       <div className={s.title}>Food</div>
     </div>
   );
--- src/components/UI/profileButton/ProfileButton.tsx
+++ src/components/UI/profileButton/ProfileButton.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
+import { CgProfile } from 'react-icons/cg';
 import s from './ProfileButton.module.css';
-import { ReactComponent as Profile } from '../../../assets/navigation/profile.svg';
 
 const ProfileButton: FC = () => {
   return (
     <div className={s.wrapper}>
-      <Profile className={s.icon} />
+      <h3 className={s.icon}>
+        <CgProfile />
+      </h3>
       <div className={s.title}>Profile</div>
     </div>
   );

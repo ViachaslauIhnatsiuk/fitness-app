ce27eb80c2e0a1b022be54d161cfd3aecfe4d6ee
GlebKolyano <glebkolyano38@gmail.com>
2022-08-20 20:01:21 +0300

    feat: add styles for ExerciseActive component

--- src/components/trainings/exerciseActive/ExerciseActive.tsx
+++ src/components/trainings/exerciseActive/ExerciseActive.tsx
@@ -1,30 +1,48 @@
 import React, { FC } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import s from './ExerciseActive.module.css';
 import { Button } from '../../UI/button/Button';
 import { CircleTimer } from '../../UI/circleTimer/CircleTimer';
 import { ExerciseActiveProps } from './models';
 
 const ExerciseActive: FC<ExerciseActiveProps> = ({
-  exercise: { description, img, time, title },
+  exercise: { img, time, title },
   onClickTimerHandler,
   onPrevHandler,
   onNextHandler
 }) => {
   return (
-    <div>
-      <img src={img} alt="exercise" />
-      <br />
-      <h4>{title}</h4>
-      <span>time: {time}</span>
-      <br />
-      <h4>{description}</h4>
+    <div className={s.wrapper}>
+      <div className={s.info}>
+        <img className={s.image} src={img} alt="exercise" />
+        <h1>{title}</h1>
+      </div>
       <CircleTimer
         duration={time}
         btnTitle="PAUSE"
         onClick={onClickTimerHandler}
         colors={['#7C00FF', '#7C00FF']}
+        size={110}
+        fontSize={50}
+        strokeWidth={10}
       />
-      <Button text="prev" onClick={onPrevHandler} isStyled />
-      <Button text="next" onClick={onNextHandler} isStyled />
+      <div className={s.buttons}>
+        <Button
+          text="Previous"
+          onClick={onPrevHandler}
+          isStyled
+          icon={<AiOutlineArrowLeft />}
+          customStyles={s.button}
+        />
+        <Button
+          text="Skip"
+          onClick={onNextHandler}
+          isStyled
+          icon={<AiOutlineArrowRight />}
+          iconPosition="right"
+          customStyles={s.button}
+        />
+      </div>
     </div>
   );
 };

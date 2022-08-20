a2348ece1650d65b6a3afb7e81981d40224b0c42
GlebKolyano <glebkolyano38@gmail.com>
2022-08-20 20:00:41 +0300

    feat: add styles for TrainingResult component

---
+++ src/components/trainings/resultTraining/TrainingResult.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './TrainingResult.module.css';
+import { Button } from '../../UI/button/Button';
+import { Path } from '../models';
+
+const TrainingResult = () => {
+  return (
+    <div className={s.wrapper}>
+      <img className={s.image} src="/images/result.png" alt="result" />
+      <h1 className={s.title}>Congratulations!</h1>
+      <p className={s.description}>You have completed the workout!</p>
+      <div className={s.buttons}>
+        <Button
+          path={Path.trainings}
+          text="Next to Another Workout"
+          isStyled
+          customStyles={s.button}
+        />
+        <Button path={Path.home} text="Home" isStyled customStyles={s.button_home} />
+      </div>
+    </div>
+  );
+};
+
+export { TrainingResult };

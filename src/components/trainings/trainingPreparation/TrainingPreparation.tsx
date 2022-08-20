7ecf1213440a1fd03c4cce12815ccd160e2b8016
GlebKolyano <glebkolyano38@gmail.com>
2022-08-20 20:02:43 +0300

    feat: add styles for TrainingPreparation component

--- src/components/trainings/trainingPreparation/TrainingPreparation.tsx
+++ src/components/trainings/trainingPreparation/TrainingPreparation.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import s from './TrainingPreparation.module.css';
 import { Button } from '../../UI/button/Button';
 import { CircleTimer } from '../../UI/circleTimer/CircleTimer';
 import { PREPARATION_TIME } from '../constants';
@@ -6,10 +7,12 @@ import { TrainingPreparationProps } from './models';
 
 const TrainingPreparation: FC<TrainingPreparationProps> = ({ onSkipHandler }) => {
   return (
-    <div>
-      <h1>Get Ready!</h1>
-      <CircleTimer duration={PREPARATION_TIME} colors={['#7C00FF', '#7C00FF']} />
-      <Button text="Start Over" onClick={onSkipHandler} isStyled />
+    <div className={s.preparation}>
+      <div className={s.preparation__timer}>
+        <h1>Get Ready!</h1>
+        <CircleTimer duration={PREPARATION_TIME} colors={['#7C00FF', '#7C00FF']} fontSize={70} />
+      </div>
+      <Button text="Start Over" onClick={onSkipHandler} isStyled customStyles={s.button} />
     </div>
   );
 };

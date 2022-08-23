0df16d3cd1b69a1ae28daf5d8f06868487805e8c
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-08-23 22:35:44 +0300

    feat: add helper function for fetch recipe by id

---
+++ src/models/modelRecipeById.ts
@@ -0,0 +1,89 @@
+interface IRecipeInfo {
+  id: number;
+  title: string;
+  image: string;
+  imageType: string;
+  servings: number;
+  readyInMinutes: number;
+  license: string;
+  sourceName: string;
+  sourceUrl: string;
+  spoonacularSourceUrl: string;
+  aggregateLikes: number;
+  healthScore: number;
+  spoonacularScore: number;
+  pricePerServing: number;
+  analyzedInstructions: string[];
+  cheap: boolean;
+  creditsText: string;
+  cuisines: string[];
+  dairyFree: boolean;
+  diets: string[];
+  gaps: string;
+  glutenFree: boolean;
+  instructions: string;
+  ketogenic: boolean;
+  lowFodmap: boolean;
+  occasions: string[];
+  sustainable: boolean;
+  vegan: boolean;
+  vegetarian: boolean;
+  veryHealthy: boolean;
+  veryPopular: boolean;
+  whole30: boolean;
+  weightWatcherSmartPoints: number;
+  dishTypes: string[];
+  extendedIngredients: ExtendedIngredient[];
+  summary: string;
+  winePairing: WinePairing;
+}
+
+interface ExtendedIngredient {
+  aisle: string;
+  amount: number;
+  consitency: Consitency;
+  id: number;
+  image: string;
+  measures: Measures;
+  meta: string[];
+  name: string;
+  original: string;
+  originalName: string;
+  unit: string;
+}
+
+enum Consitency {
+  Liquid = 'liquid',
+  Solid = 'solid'
+}
+
+interface Measures {
+  metric: Metric;
+  us: Metric;
+}
+
+interface Metric {
+  amount: number;
+  unitLong: string;
+  unitShort: string;
+}
+
+interface WinePairing {
+  pairedWines: string[];
+  pairingText: string;
+  productMatches: ProductMatch[];
+}
+
+interface ProductMatch {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  imageUrl: string;
+  averageRating: number;
+  ratingCount: number;
+  score: number;
+  link: string;
+}
+
+export type { IRecipeInfo };

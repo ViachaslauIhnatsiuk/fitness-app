139f8c797a48062ca99b46b0eb45aca3f1bd0893
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-08-23 23:40:34 +0300

    feat: add Recipe

--- src/components/recipes/recipe/Recipe.tsx
+++ src/components/recipes/recipe/Recipe.tsx
@@ -1,18 +1,51 @@
-import React, { FC } from 'react';
-import { RecipeProps } from './models';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchRecipeByID } from '../../../helpers/fetchRecipeById';
+import { IRecipeInfo } from '../../../models/modelRecipeById';
 import s from './Recipe.module.css';
 
-const Recipe: FC<RecipeProps> = ({ data: { image, label, dishType, calories } }) => {
+const Recipe = () => {
+  const [recipeInfo, setRecipeInfo] = useState<IRecipeInfo>({} as IRecipeInfo);
+  const params = useParams();
+  const recipeId = params.recipeId || '';
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchRecipeByID(recipeId);
+      setRecipeInfo(data);
+    })().catch(() => {});
+  }, [recipeId]);
+
   return (
-    <div className={s.wrapper}>
-      <div className={s.image_wrapper}>
-        <img src={image} className={s.image} alt={label} />
-      </div>
-      <div className={s.overlay}>
-        <span className={s.label}>{label}</span>
-        <span className={s.label}>Type: {dishType}</span>
-        <span className={s.label}>Calories: {calories.toFixed(1)} cal</span>
-      </div>
+    <div>
+      {Object.keys(recipeInfo).length && (
+        <div className={s.recipe_info}>
+          <h3 className={s.title}>{recipeInfo.title}</h3>
+          <img className={s.image} src={recipeInfo.image} />
+          <div>
+            <span>Ready In Minutes: {recipeInfo.readyInMinutes}</span>
+            <div>
+              Types:
+              {recipeInfo.dishTypes?.map((dishType) => (
+                <span> {dishType}</span>
+              ))}
+            </div>
+            <div className={s.ingredients}>
+              Ingredients:
+              {recipeInfo.extendedIngredients.map((ingredient) => (
+                <div className={s.ingredient}>
+                  <span>{ingredient.name}</span>
+                  <span>: </span>
+                  <span>{ingredient.measures.metric.amount}</span>
+                  <span> - </span>
+                  <span>{ingredient.measures.metric.unitLong}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <p className={s.summary}>{recipeInfo.summary}</p>
+        </div>
+      )}
     </div>
   );
 };

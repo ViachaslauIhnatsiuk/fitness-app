2a454b25e736ac440cc6ab6e31fd8e8bbc5e095c
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-08-26 19:01:17 +0300

    feat: handle google auth, refactor: refactor component for updating profile

--- src/components/editProfile/EditProfile.tsx
+++ src/components/editProfile/EditProfile.tsx
@@ -1,64 +1,41 @@
 import React, { FC } from 'react';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { IoChevronBackCircleOutline } from 'react-icons/io5';
-import { IUserProfile } from '../registration/registrationUserProfile/models';
-import { nameRegister, emailRegister, passwordRegister } from '../../constants/formValidation';
-import { Button } from '../UI/button/Button';
+import { IoMdClose } from 'react-icons/io';
 import { useProfileUpdate } from '../../hooks/useProfileUpdate';
+import { NameInput } from '../UI/nameInput/NameInput';
+import { EmailInput } from '../UI/emailInput/EmailInput';
+import { PasswordInput } from '../UI/passwordInput/PasswordInput';
+import { RegSubmitButton } from '../UI/submitButtons/regSubmitButton/RegSubmitButton';
+import { IUserProfile } from '../registration/registrationUserProfile/models';
+import { Button } from '../UI/button/Button';
 import s from './EditProfile.module.css';
 
 const EditProfile: FC = () => {
-  const { updateUserProfile } = useProfileUpdate();
-  const {
-    register,
-    handleSubmit,
-    reset,
-    getValues,
-    formState: { errors, isValid }
-  } = useForm<IUserProfile>({ mode: 'onBlur' });
+  const { success, setSuccess, updateUserProfile } = useProfileUpdate();
+  const methods = useForm<IUserProfile>({ mode: 'onBlur' });
+  const { handleSubmit, reset } = methods;
 
   return (
     <div className={s.wrapper}>
       <Button path="/profile" icon={<IoChevronBackCircleOutline />} />
       <div className={s.title}>Edit Profile</div>
-      <form className={s.form} onSubmit={handleSubmit(() => reset())}>
-        <div className={s.field}>
-          <input
-            autoComplete="off"
-            className={s.input}
-            placeholder="Name"
-            {...register('name', nameRegister)}
-          />
-          <p className={s.warning}>{errors.name?.message}</p>
+      {success && (
+        <div className={s.notification}>
+          <div className={s.text}>
+            Congratulations, your profile data has been successfully updated
+          </div>
+          <IoMdClose className={s.close} onClick={() => setSuccess(false)} />
         </div>
-        <div className={s.field}>
-          <input
-            autoComplete="off"
-            className={s.input}
-            placeholder="E-mail"
-            type="email"
-            {...register('email', emailRegister)}
-          />
-          <p className={s.warning}>{errors.email?.message}</p>
-        </div>
-        <div className={s.field}>
-          <input
-            className={s.input}
-            placeholder="Password"
-            type="password"
-            {...register('password', passwordRegister)}
-          />
-          <p className={s.warning}>{errors.password?.message}</p>
-        </div>
-        <input
-          className={s.button}
-          style={{ backgroundColor: !isValid ? '#35383f' : '#7755ff' }}
-          disabled={!isValid}
-          type="submit"
-          value="Update"
-          onClick={() => updateUserProfile(...getValues(['name', 'email', 'password']))}
-        />
-      </form>
+      )}
+      <FormProvider {...methods}>
+        <form className={s.form} onSubmit={handleSubmit(() => reset())}>
+          <NameInput />
+          <EmailInput />
+          <PasswordInput />
+          <RegSubmitButton path="" value="Update" handler={updateUserProfile} />
+        </form>
+      </FormProvider>
     </div>
   );
 };

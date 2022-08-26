d2ebf1046ad89b0ef7a22da62dbde9bc74378014
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-08-26 23:12:00 +0300

    feat: add the ability to view entered password

--- src/components/UI/confirmPasswordInput/ConfirmPasswordInput.tsx
+++ src/components/UI/confirmPasswordInput/ConfirmPasswordInput.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { passwordRegister, matchPasswordReport } from '../../../constants/formValidation';
 import { IUserProfile } from '../../registration/registrationUserProfile/models';
 import s from './ConfirmPasswordInput.module.css';
 
 const ConfirmPasswordInput: FC = () => {
+  const [passwordType, setPasswordType] = useState<string>('password');
   const {
     register,
     watch,
@@ -22,6 +24,11 @@ const ConfirmPasswordInput: FC = () => {
           validate: (value) => value === watch('password') || matchPasswordReport
         })}
       />
+      {passwordType === 'password' ? (
+        <AiFillEyeInvisible className={s.icon} onClick={() => setPasswordType('text')} />
+      ) : (
+        <AiFillEye className={s.icon} onClick={() => setPasswordType('password')} />
+      )}
       <p className={s.warning}>{errors.confirmPassword?.message}</p>
     </div>
   );
--- src/components/UI/passwordInput/PasswordInput.tsx
+++ src/components/UI/passwordInput/PasswordInput.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { passwordRegister } from '../../../constants/formValidation';
 import { IUserProfile } from '../../registration/registrationUserProfile/models';
 import s from './PasswordInput.module.css';
 
 const PasswordInput: FC = () => {
+  const [passwordType, setPasswordType] = useState<string>('password');
   const {
     register,
     formState: { errors }
@@ -15,9 +17,14 @@ const PasswordInput: FC = () => {
       <input
         className={s.input}
         placeholder="Password"
-        type="password"
+        type={passwordType}
         {...register('password', passwordRegister)}
       />
+      {passwordType === 'password' ? (
+        <AiFillEyeInvisible className={s.icon} onClick={() => setPasswordType('text')} />
+      ) : (
+        <AiFillEye className={s.icon} onClick={() => setPasswordType('password')} />
+      )}
       <p className={s.warning}>{errors.password?.message}</p>
     </div>
   );

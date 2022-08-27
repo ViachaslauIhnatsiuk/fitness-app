3dd14983c59ecca959b2c7c9de507d599235ee0c
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-08-27 12:27:18 +0300

    refactor: minor refactor recipesSlice

---
+++ src/store/slices/recipes/model.ts
@@ -0,0 +1,28 @@
+interface IRecipe {
+  id: number;
+  title: string;
+  image: string;
+  imageType: string;
+}
+
+type RecipesRequestConfig = {
+  query: string;
+  type: string;
+};
+
+interface RecipeResponse {
+  results: IRecipe[];
+  offset: number;
+  number: number;
+  totalResults: number;
+}
+
+interface RecipesState {
+  recipes: RecipeResponse;
+  isLoading: boolean;
+  isUploaded: boolean;
+  queryParams: RecipesRequestConfig;
+  error: string;
+}
+
+export type { IRecipe, RecipesRequestConfig, RecipeResponse, RecipesState };
---
+++ src/store/slices/recipes/recipesSlice.ts
@@ -0,0 +1,60 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { API_KEY, BASE_URL } from '../../constants';
+import type { RecipeResponse, RecipesState, RecipesRequestConfig } from './model';
+import { getMixedQueries } from '../../../helpers/getMixedQueries';
+
+const initialState: RecipesState = {
+  recipes: {
+    results: [],
+    offset: 0,
+    number: 0,
+    totalResults: 0
+  },
+  isLoading: false,
+  isUploaded: false,
+  queryParams: {
+    query: '',
+    type: ''
+  },
+  error: ''
+};
+
+export const fetchRecipes = createAsyncThunk(
+  'recipes/fetchRecipes',
+  async (config: RecipesRequestConfig, { rejectWithValue }) => {
+    try {
+      const mixedQueries = getMixedQueries(config);
+      const response = await fetch(
+        `${BASE_URL}/complexSearch?${mixedQueries}&apiKey=${API_KEY}&number=12`
+      ).catch((error: Error) => error);
+      const data = (await (response as Response).json()) as RecipeResponse;
+      return { recipes: data, queryParams: config } as RecipesState;
+    } catch (error) {
+      return rejectWithValue('Error fetching recipes');
+    }
+  }
+);
+
+const recipesSlice = createSlice({
+  name: 'recipes',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchRecipes.fulfilled.type]: (state, action) => {
+      state.recipes = action.payload.recipes;
+      state.isLoading = false;
+      state.isUploaded = true;
+      state.queryParams = action.payload.queryParams;
+      state.error = '';
+    },
+    [fetchRecipes.pending.type]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchRecipes.rejected.type]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    }
+  }
+});
+
+export { recipesSlice };

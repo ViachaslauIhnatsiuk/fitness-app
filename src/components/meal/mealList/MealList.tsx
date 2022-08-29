e16818b028a8e2db920a06ad8264124a405a1060
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-08-29 18:01:15 +0300

    fix: fix DND order

--- src/components/meal/mealList/MealList.tsx
+++ src/components/meal/mealList/MealList.tsx
@@ -7,14 +7,13 @@ import {
   DraggableLocation
 } from 'react-beautiful-dnd';
 import { MealCard } from '../mealCard/MealCard';
-import { MealCardProps } from '../mealCard/models';
 import { mealsTitles } from './constants';
 import './MealList.css';
 
 const MealList: FC = () => {
-  const [cardsOrder, updateCardsOrder] = useState<MealCardProps[]>(mealsTitles);
+  const [cardsOrder, updateCardsOrder] = useState<{ id: number; title: string }[]>(mealsTitles);
 
-  const handleonDragEnd = (result: DropResult): void => {
+  const handleOnDragEnd = (result: DropResult): void => {
     const cards = Array.from(cardsOrder);
     const [reorderedCards] = cards.splice(result.source.index, 1);
     cards.splice((result.destination as DraggableLocation).index, 0, reorderedCards);
@@ -22,18 +21,18 @@ const MealList: FC = () => {
   };
 
   return (
-    <DragDropContext onDragEnd={handleonDragEnd}>
-      <Droppable droppableId="card_wrapper" direction="horizontal">
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="list_wrapper" direction="horizontal">
         {(provided) => (
           <div className="list_wrapper" {...provided.droppableProps} ref={provided.innerRef}>
-            {mealsTitles.map(({ title, id }, index) => {
+            {cardsOrder.map(({ title, id }, index) => {
               return (
-                <Draggable key={id} draggableId={(id as number).toString()} index={index}>
-                  {(provided2) => (
+                <Draggable key={id} draggableId={id.toString()} index={index}>
+                  {(provider) => (
                     <div
-                      ref={provided2.innerRef}
-                      {...provided2.draggableProps}
-                      {...provided2.dragHandleProps}
+                      ref={provider.innerRef}
+                      {...provider.draggableProps}
+                      {...provider.dragHandleProps}
                     >
                       <MealCard title={title} />
                     </div>

ba703b0e56713214f614063760f1502c8e4338d1
GlebKolyano <glebkolyano38@gmail.com>
2022-08-29 10:20:03 +0300

    refactor: refactor components

--- src/components/favorite/favoriteCategory/FavoriteCategory.tsx
+++ src/components/favorite/favoriteCategory/FavoriteCategory.tsx
@@ -1,29 +1,16 @@
 import React, { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import s from './FavoriteCategory.module.css';
 import { FavoriteCardProps } from './models';
 
 const FavoriteCategory: FC<FavoriteCardProps> = ({ favorite: { image, redirectPath, title } }) => {
-  const navigate = useNavigate();
-
-  const openFavoriteHandler = () => {
-    const path = redirectPath;
-    navigate(path);
-  };
-
   return (
-    <div
-      className={s.wrapper}
-      onClick={openFavoriteHandler}
-      onKeyPress={openFavoriteHandler}
-      role="link"
-      tabIndex={0}
-    >
+    <Link className={s.wrapper} to={redirectPath}>
       <h3 className={s.title}>{title}</h3>
       <div className={s.image}>
         <img src={image} alt={title} />
       </div>
-    </div>
+    </Link>
   );
 };
 

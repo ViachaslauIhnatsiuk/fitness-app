ab68e69fe3e28fa057ae1b06671d31e9e6fbba59
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-08-30 11:57:41 +0300

    refactor: refactor new dish component

--- src/components/meal/newDish/NewDish.tsx
+++ src/components/meal/newDish/NewDish.tsx
@@ -1,12 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { IoAdd } from 'react-icons/io5';
 import s from './NewDish.module.css';
 
 const NewDish: FC = () => {
+  const [dishName, setDishName] = useState<string>('');
+  const [dishSize, setDishSize] = useState<number>();
+
   return (
     <div className={s.wrapper}>
-      <div className={s.text}>Add new dish</div>
-      <IoAdd className={s.icon} />
+      <input
+        type="text"
+        className={s.dish}
+        placeholder="Dish"
+        value={dishName}
+        onChange={(e) => setDishName(e.target.value)}
+      />
+      <input
+        type="number"
+        className={s.size}
+        placeholder="Dish size"
+        value={dishSize}
+        onChange={(e) => setDishSize(Number(e.target.value))}
+      />
+      <button type="submit" className={s.button}>
+        <IoAdd className={s.icon} />
+      </button>
     </div>
   );
 };

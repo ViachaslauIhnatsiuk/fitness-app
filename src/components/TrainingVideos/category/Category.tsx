923d8a5b35cfdd204272cded5c4e51a3921b640d
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-08-31 23:00:31 +0300

    refactor: refactor workout categories

--- src/components/TrainingVideos/category/Category.tsx
+++ src/components/TrainingVideos/category/Category.tsx
@@ -1,16 +1,10 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { useStorage } from '../../../hooks/useStorage';
 import { CategoryProps } from './models';
 import s from './Category.module.css';
 
 const Category: FC<CategoryProps> = ({ category }) => {
-  const { categoryImageUrl, getCategoryImageUrl } = useStorage();
   const redirectPath = `${category}/`;
-
-  useEffect(() => {
-    getCategoryImageUrl(category).catch((error: Error) => error);
-  }, [getCategoryImageUrl, category]);
 
   return (
     <Link className={s.wrapper} to={redirectPath}>

ef3512da76e60b1adc8f41b75d707e0d73f9842e
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-09-02 21:20:28 +0300

    feat: add progress bar

--- src/components/meal/mealNutrition/MealNutritionCard.tsx
+++ src/components/meal/mealNutrition/MealNutritionCard.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import ProgressBar from '@ramonak/react-progress-bar';
 import { NutritionCardProps } from './models';
 import s from './MealNutritionCard.module.css';
 import { IConsumption } from '../../../models/Consumption';
@@ -7,6 +8,7 @@ import { setTitleCase } from '../../../helpers/setTitleCase';
 const MealNutritionCard: FC<NutritionCardProps> = ({ title, curScore, maxScore }) => {
   const currentValue = curScore[title as keyof IConsumption];
   const maxValue = maxScore[title as keyof IConsumption];
+  const percentCompleted = Math.round((currentValue / maxValue) * 100);
 
   return (
     <div className={s.wrapper}>
@@ -16,7 +18,16 @@ const MealNutritionCard: FC<NutritionCardProps> = ({ title, curScore, maxScore }
           {currentValue} / {maxValue}
         </div>
       </div>
-      <div className={s.progress} />
+      <div className={s.progress}>
+        <ProgressBar
+          completed={percentCompleted}
+          bgColor="#7755ff"
+          height="8px"
+          width="100%"
+          borderRadius="10px"
+          baseBgColor="#e5e5e5"
+        />
+      </div>
     </div>
   );
 };

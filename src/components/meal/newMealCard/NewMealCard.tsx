2712fa451cbf80f3f9dc84636146ff1312989415
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-09-02 13:47:39 +0300

    feat: add newMealCard component

--- src/components/meal/newMealCard/NewMealCard.tsx
+++ src/components/meal/newMealCard/NewMealCard.tsx
@@ -1,13 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import { IoAdd } from 'react-icons/io5';
+import { addCardToUserMeals } from '../../../store/slices/profileSlice';
+import { useAppDispatch } from '../../../store/store';
 import s from './NewMealCard.module.css';
 
 const NewMealCard: FC = () => {
+  const [newTitle, setNewTitle] = useState<string>('');
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (newTitle.length > 3) {
+      dispatch(addCardToUserMeals(newTitle));
+      setNewTitle('');
+    }
+  };
   return (
     <div className={s.wrapper}>
-      <div className={s.circle}>
-        <IoAdd className={s.icon} />
-      </div>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <input
+          type="text"
+          className={s.input}
+          placeholder="Enter new card title"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+        />
+        <button type="submit" className={s.button}>
+          <IoAdd className={s.icon} />
+        </button>
+      </form>
     </div>
   );
 };

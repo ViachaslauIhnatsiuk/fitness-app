5d41a1b410c87b50672b4175ee17ab049a0bb8df
Viachaslau <viachaslauihnatsiuk@gmail.com>
2022-09-05 20:26:34 +0300

    refactor: refactor video trainings

--- src/components/UI/videoPlayer/VideoPlayer.tsx
+++ src/components/UI/videoPlayer/VideoPlayer.tsx
@@ -4,12 +4,14 @@ import s from './VideoPlayer.module.css';
 
 const VideoPlayer: FC<VideoPlayerProps> = ({ previewUrl, videoUrl }) => {
   return (
-    <video className={s.video} controls poster={previewUrl} preload="none">
-      <track kind="captions" />
-      {videoUrl && <source src={videoUrl} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />}
-      Тег video не поддерживается вашим браузером.
-      {videoUrl && <a href={videoUrl}>Скачайте видео</a>}
-    </video>
+    <div className={s.wrapper}>
+      <video className={s.video} controls poster={previewUrl} preload="none">
+        <track kind="captions" />
+        {videoUrl && <source src={videoUrl} type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />}
+        Тег video не поддерживается вашим браузером.
+        {videoUrl && <a href={videoUrl}>Скачайте видео</a>}
+      </video>
+    </div>
   );
 };
 
